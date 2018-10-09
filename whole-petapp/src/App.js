a6b4228b8e6a4f7c7c4ee33e038c99dc979190f0
irinafedorovacisc <true83@list.ru>
2018-10-08 20:10:43 -0400

    first front end commit login form

---
+++ whole-petapp/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Login from './components/Login/Login';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>          
+          <Login />  
+      </div>
+    );
+  }
+}
+
+export default App;

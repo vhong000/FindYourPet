5810038d695b64d73a35ae0221f5c5103be125bb
Victor Hong <victorhong04@gmail.com>
2018-10-23 22:44:35 -0400

    routing in index.js file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import {BrowserRouter,Route,Switch} from "react-router-dom";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage';
+import NavBar from './components/NavBar';
+import FindPetPage from './components/FindPetPage';
+import AdopterProfile from './components/AdopterProfile';
+import AdopteeProfile from './components/AdopteeProfile';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<BrowserRouter>
+		<div>
+			<NavBar/> 
+			<Switch>
+				<Route exact path='/' component={HomePage} />
+				<Route path='/HomePage' component={HomePage} />
+				<Route path='/FindPetPage' component={FindPetPage} />
+				<Route path='/LoginPage' component={LoginPage} />
+				<Route path='/AdopterProfile' component={AdopterProfile} />
+				<Route path='/AdopteeProfile' component={AdopteeProfile} />
+			</Switch>
+		</div>
+	</BrowserRouter>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

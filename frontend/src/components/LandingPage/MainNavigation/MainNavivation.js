5d6455b2c6b76b758b1502f8c773ac996b8ff53e
Daniel Constantine <danielkevinconstantine@gmail.com>
2018-11-01 22:14:19 -0400

    making the navigation bar inline and the hamburger icon

--- frontend/src/components/LandingPage/MainNavigation/MainNavivation.js
+++ frontend/src/components/LandingPage/MainNavigation/MainNavivation.js
@@ -1,37 +1,49 @@
-import React, { Component } from 'react';
-import './MainNavigation.css'
+import React, { Component } from "react";
+import "./MainNavigation.css";
 
 export default class MainNavigation extends Component {
   render() {
     return (
       <div>
-          <div className="row">
+        <div className="row">
           <div className="col-md-4 mb-3 mb-md-0">
-              <div className="card py-4 h-100">
-                  <div className="card-body text-center">                                                                    
-                      <h4 className="text-uppercase m-0">FIND CATS</h4>                                         
-                      <img className="img-fluid mb-5 d-block mx-auto" src="" alt="cat image"/>                  
-                  </div>
-              </div>            
+            <div className="card py-4 h-100">
+              <div className="card-body text-center">
+                <h4 className="text-uppercase m-0">FIND CATS</h4>
+                <img
+                  className="img-fluid mb-5 d-block mx-auto"
+                  src=""
+                  alt="cat image"
+                />
+              </div>
+            </div>
           </div>
           <div className="col-md-4 mb-3 mb-md-0">
-              <div className="card py-4 h-100">
-                  <div className="card-body text-center">                                                                    
-                      <h4 className="text-uppercase m-0">FIND DOGS</h4>                                         
-                      <img className="img-fluid mb-5 d-block mx-auto" src="" alt="DOG image"/>                  
-                  </div>
-              </div>            
+            <div className="card py-4 h-100">
+              <div className="card-body text-center">
+                <h4 className="text-uppercase m-0">FIND DOGS</h4>
+                <img
+                  className="img-fluid mb-5 d-block mx-auto"
+                  src=""
+                  alt="DOG image"
+                />
+              </div>
+            </div>
           </div>
           <div className="col-md-4 mb-3 mb-md-0">
-              <div className="card py-4 h-100">
-                  <div className="card-body text-center">                                                                    
-                      <h4 className="text-uppercase m-0">FIND ME</h4>                                         
-                      <img className="img-fluid mb-5 d-block mx-auto" src="" alt="ME image"/>                  
-                  </div>
-              </div>            
-          </div>      
-          </div>        
+            <div className="card py-4 h-100">
+              <div className="card-body text-center">
+                <h4 className="text-uppercase m-0">FIND ME</h4>
+                <img
+                  className="img-fluid mb-5 d-block mx-auto"
+                  src=""
+                  alt="ME image"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
--- frontend/src/components/MainDashboard/SearchingCriterias/SearchingCriterias.js
+++ frontend/src/components/MainDashboard/SearchingCriterias/SearchingCriterias.js
@@ -1,39 +1,68 @@
-import React, { Component } from 'react';
-import './SearchingCriterias.css';
+import React, { Component } from "react";
+import "./SearchingCriterias.css";
 
 export default class SearchingCriterias extends Component {
   render() {
     return (
       <div>
         <div className="card my-4">
-            <h5 className="card-header">Searching criterias:</h5>
-            <div className="card-body">
-                <div className="input-group">
-                    <div className="btn-group btn-group-toggle" data-toggle="buttons">
-                            <label className="btn btn-secondary active">
-                            <input type="radio" name="options" id="option1" autocomplete="off" checked /> Dog
-                            </label>
-                        
-                            <label className="btn btn-secondary">
-                            <input type="radio" name="options" id="option2" autocomplete="off" /> Cat
-                            </label>                           
-                    </div>
-                
-                    <div className="dropdown" id="catdog">
-                        <br/>
-                            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Sort By
-                            </button>
-                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a className="dropdown-item" href="#">Age</a>
-                            <a className="dropdown-item" href="#">Zip</a>
-                            <a className="dropdown-item" href="#">Bread</a>
-                            </div>
-                    </div>             
+          <h5 className="card-header">Searching criterias:</h5>
+          <div className="card-body">
+            <div className="input-group">
+              <div className="btn-group btn-group-toggle" data-toggle="buttons">
+                <label className="btn btn-secondary active">
+                  <input
+                    type="radio"
+                    name="options"
+                    id="option1"
+                    autocomplete="off"
+                    checked
+                  />{" "}
+                  Dog
+                </label>
+
+                <label className="btn btn-secondary">
+                  <input
+                    type="radio"
+                    name="options"
+                    id="option2"
+                    autocomplete="off"
+                  />{" "}
+                  Cat
+                </label>
+              </div>
+
+              <div className="dropdown" id="catdog">
+                <br />
+                <button
+                  className="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  id="dropdownMenuButton"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Sort By
+                </button>
+                <div
+                  className="dropdown-menu"
+                  aria-labelledby="dropdownMenuButton"
+                >
+                  <a className="dropdown-item" href="#">
+                    Age
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Zip
+                  </a>
+                  <a className="dropdown-item" href="#">
+                    Breed
+                  </a>
                 </div>
+              </div>
             </div>
-        </div>        
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }

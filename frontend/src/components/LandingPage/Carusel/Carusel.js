975337f9d726c5e7537fdab338a8481b5648ad8e
Daniel Constantine <danielkevinconstantine@gmail.com>
2018-11-06 11:21:18 -0500

    update

--- frontend/src/components/LandingPage/Carusel/Carusel.js
+++ frontend/src/components/LandingPage/Carusel/Carusel.js
@@ -1,6 +1,9 @@
-
 import React, { Component } from "react";
 import "./Carusel.css";
+
+import logo1 from "../../../Images/logo1.png";
+import logo2 from "../../../Images/logo2.png";
+import logo3 from "../../../Images/logo3.png";
 
 export default class Carusel extends Component {
   render() {
@@ -19,12 +22,15 @@ export default class Carusel extends Component {
           <li data-target="#carouselExampleIndicators" data-slide-to="1" />
           <li data-target="#carouselExampleIndicators" data-slide-to="2" />
         </ol>
-        <div className="carousel-inner" >
-          <div className="carousel-item active"  id="pic1">
+        <div className="carousel-inner">
+          <div className="carousel-item active" id="pic1">
+            <img className="d-block w-100" src={logo1} alt="First slide" />
           </div>
-          <div className="carousel-item"  id="pic1">
+          <div className="carousel-item" id="pic2">
+            <img className="d-block w-100" src={logo2} alt="Second slide" />
           </div>
-          <div className="carousel-item"  id="pic1">
+          <div className="carousel-item" id="pic3">
+            <img className="d-block w-100" src={logo3} alt="Third slide" />
           </div>
         </div>
         <a

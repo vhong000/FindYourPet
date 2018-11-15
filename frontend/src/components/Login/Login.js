fcab26205494627bb6c0247b171a9bd30200eb4e
Daniel Constantine <danielkevinconstantine@gmail.com>
2018-11-15 18:39:08 -0500

    success login, stuck in one page

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,69 +1,103 @@
-import React, { Component } from 'react';
-import './Login.css';
-
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
+import "./Login.css";
 
 const sendData = {
-    email: "",
-    password: "",
+  email: "",
+  password: "",
 
-    send(email,password){
-        fetch('/api/auth/login',{
-            method:'POST',
-            headers:{
-                "Content-Type": "application/json; charset=utf-8",
-            },
-        body: JSON.stringify({
-            email:email,
-            password:password
-        }),
-        }).then((response) => {
-            console.log(response.status)
-            if(response.status === 200){
-                console.log('success')
-            }
-        });
-    }
-}
-
+  send(email, password, cb) {
+    fetch("/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    })
+      .then(response => {
+        console.log(response.status);
+        if (response.status === 200) {
+          console.log("success");
+        }
+        //   this.setState({ redirectToReferrer: true });
+      })
+      .then(body => {
+        // console.log(body);
+        cb();
+        // return;
+      });
+  }
+};
 
 export default class Login extends Component {
+  state = {
+    redirectToReferrer: false,
+    email: "",
+    password: ""
+  };
 
-    state = { 
-        email: "",
-        password: "",
-    };
+  submit = () => {
+    sendData.send(this.state.email, this.state.password, () => {
+      this.setState({ redirectToReferrer: true });
+    });
+  };
 
-    submit = () => {
-        sendData.send(this.state.email, this.state.password);
-    }
+  handleEmailChange = event => {
+    this.setState({ email: event.target.value });
+  };
 
-    handleEmailChange = (event) => {
-        this.setState({ email: event.target.value });
-    }
+  handlePassChange = event => {
+    this.setState({ password: event.target.value });
+  };
 
-    handlePassChange = (event) => {
-        this.setState({ password: event.target.value });
-    }
+  render() {
+    let { from } = { pathname: "/" };
+    let { redirectToReferrer } = this.state;
 
-    render() {
+    if (redirectToReferrer) return <Redirect to={from} />;
+
     return (
-        <div className="panel">            
-            <form id="loginform">
-            <h2>login</h2> 
-            <br/>         
-            <div className="form-group">
-                <input type="email" className="form-control" id="inputEmail" placeholder="Email Address" onChange={this.handleEmailChange} value={this.state.email}/>
-            </div>
-            <div className="form-group">
-                <input type="password" className="form-control" id="inputPassword" placeholder="Password" onChange={this.handlePassChange} value={this.state.password}/>
-            </div>
-            <div className="forgot">
-                <a href="reset.html">Forgot password?</a>               																	
-            </div>                        
-            </form>
-            <button type="submit" className="btn btn" onClick={this.submit}>Login</button>    
-        </div>       
-        )
-    }
+      <div className="panel">
+        <form id="loginform">
+          <h2>login</h2>
+          <br />
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="inputEmail"
+              placeholder="Email Address"
+              onChange={this.handleEmailChange}
+              value={this.state.email}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword"
+              placeholder="Password"
+              onChange={this.handlePassChange}
+              value={this.state.password}
+            />
+          </div>
+          <div className="forgot">
+            <a href="reset.html">Forgot password?</a>
+          </div>
+        </form>
+        <button type="submit" className="btn btn" onClick={this.submit}>
+          Login
+        </button>
+      </div>
+    );
+  }
 }
-

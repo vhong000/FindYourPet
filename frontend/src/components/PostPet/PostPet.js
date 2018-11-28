30a147cae6c919627176a61ddc46847357111bb3
Daniel Constantine <danielkevinconstantine@gmail.com>
2018-11-27 19:07:58 -0500

    Redirecting from posting to findpetpage

--- frontend/src/components/PostPet/PostPet.js
+++ frontend/src/components/PostPet/PostPet.js
@@ -1,47 +1,44 @@
-import React, { Component } from 'react';
-import Settings from '../Profiles/Settings/Settings';
-import PetImages from '../Profiles/AdopteeProfile/PetImages';
-import PostButton from './PostButton';
-import './PostPet.css';
-import PetInformation from '../Profiles/AdopteeProfile/PetInformation';
-import EnergyLevel from './EnergyLevel';
-import {Redirect} from 'react-router-dom';
-
+import React, { Component } from "react";
+import Settings from "../Profiles/Settings/Settings";
+import PetImages from "../Profiles/AdopteeProfile/PetImages";
+import PostButton from "./PostButton";
+import "./PostPet.css";
+import PetInformation from "../Profiles/AdopteeProfile/PetInformation";
+import EnergyLevel from "./EnergyLevel";
+import { Redirect } from "react-router-dom";
 
 export default class PostPet extends Component {
   render() {
-    if(this.props.auth === false) {
-        return <Redirect to={"/"}/>
+    if (this.props.auth === false) {
+      return <Redirect to={"/"} />;
     } else {
-        return (
-            <div class="container">   
-        <br/>
-        <h2>Post a Pet</h2>
-            <div class="row">
-                <div class="col-md-3 ">
-                    <Settings />
-                </div>
-                <div class="col-md-9">
-                    <div class="card">
-                        <div class="card-body">                    
-                           <div class="row">
-                                <div class="col-md-12">                                
-                                
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12">                                                    
-                                    <PetImages />  
-                                    <PetInformation/>
-                                    <EnergyLevel />                     
-                                </div>
-                            </div>                
-                        </div>
-                    </div>
-                </div>
+      return (
+        <div class="container">
+          <br />
+          <h2>Post a Pet</h2>
+          <div class="row">
+            <div class="col-md-3 ">
+              <Settings />
             </div>
+            <div class="col-md-9">
+              <div class="card">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-md-12" />
+                  </div>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <PetImages />
+                      <PetInformation history={this.props.history} />
+                      <EnergyLevel />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        )
+      );
     }
   }
 }

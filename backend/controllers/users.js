323c285b8343052254497f4488f050bd12bcbb15
Victor Hong <victorhong04@gmail.com>
2018-12-01 23:48:58 -0500

    interested pets routes

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -6,6 +6,19 @@ const User = models.User;
 
 const router = express.Router();
 
+router.post('/interested', (req, res) => {
+	if (req.user) {
+		Pet.findById(req.body.petId).then((pet) => {
+			if (pet.userId === req.user.id) {
+				res.sendStatus(400);
+			} else {
+				req.user.addInterestedPets(pet).then(() => res.json({ msg: "requested interest" }))
+					.catch(e => res.sendStatus(500));
+			}
+		})
+	} else { res.sendStatus(404); }
+})
+
 router.get('/interested', (req, res) => {
 	if (req.user) {
 		req.user.getInterestedPets().then(pets => { res.json(pets); })

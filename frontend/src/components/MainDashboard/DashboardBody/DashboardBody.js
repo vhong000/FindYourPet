eba5593624f459cfc45449160fac27ac9be1320a
Victor Hong <victorhong04@gmail.com>
2018-12-03 23:22:31 -0500

    moved petcard into own file user profile list pets

--- frontend/src/components/MainDashboard/DashboardBody/DashboardBody.js
+++ frontend/src/components/MainDashboard/DashboardBody/DashboardBody.js
@@ -4,35 +4,8 @@ import UserCard from "../UserCard/UserCard";
 import Pagination from "../Pagination/Pagination";
 import SearchingCriterias from "../SearchingCriterias/SearchingCriterias";
 import LikedPets from "../LikedPets/LikedPets";
-import cardphoto from "../../../Images/paw.png";
+import PetCard from '../../PetProfile/PetCard';
 import "./DashboardBody.css";
-
-function PetCard(props) {
-  return (
-    <div className="col-md-4 mb-3 mb-md-0" id="usercard">
-      <div className="card h-100">
-        <h4 className="text-uppercase m-0 card-header">{props.name}</h4>
-        <div className="card-body text-center">
-          <hr className="my-4" />
-          <img
-            className="img-fluid mb-5 d-block mx-auto"
-            src={cardphoto}
-            alt="usercard"
-          />
-          <div className="small text-black-50">
-            <ul className="card-text">
-              <li> Species: {props.species}</li>
-              <li> Breed: {props.breed}</li>
-              <li> Gender: {props.gender}</li>
-              <li> DoB: {props.dob}</li>
-              <li> description: {props.description}</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
 
 export default class DashboardBody extends Component {
   constructor(props) {

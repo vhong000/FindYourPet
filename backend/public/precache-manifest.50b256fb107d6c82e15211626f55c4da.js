49453f5279813e26885d9d75ff6b94b070892eed
Joel <francoj1000@gmail.com>
2019-05-15 08:47:49 -0400

    New build file

---
+++ backend/public/precache-manifest.50b256fb107d6c82e15211626f55c4da.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "3207311938de0d03acf12e98f9e12424",
+    "url": "/static/media/find-dog.32073119.png"
+  },
+  {
+    "revision": "ea3506b1336d63814bb8",
+    "url": "/static/css/main.5583ea3b.chunk.css"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "e6bb5eed844c155b0f32",
+    "url": "/static/js/1.e6bb5eed.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4aae791f3777810ad0e1be43cdc766fe",
+    "url": "/static/media/paw.4aae791f.png"
+  },
+  {
+    "revision": "2a71914d80a0b761d6834f2cf112bd59",
+    "url": "/static/media/interlaced.2a71914d.png"
+  },
+  {
+    "revision": "830a342a973d756b63b01496c543f1d5",
+    "url": "/static/media/logo1.830a342a.png"
+  },
+  {
+    "revision": "ea3506b1336d63814bb8",
+    "url": "/static/js/main.ea3506b1.chunk.js"
+  },
+  {
+    "revision": "97758776ae12e55d2a98ca33af005403",
+    "url": "/static/media/find-cat.97758776.png"
+  },
+  {
+    "revision": "935f96a9691050d60baed9d7b0c706c1",
+    "url": "/static/media/post-pet.935f96a9.png"
+  },
+  {
+    "revision": "5ff5b784a9616220e62ae555ffc9467c",
+    "url": "/static/media/victor.5ff5b784.JPG"
+  },
+  {
+    "revision": "68af0d49c737d14a8b4bd17b4f95d94d",
+    "url": "/static/media/Joel.68af0d49.jpg"
+  },
+  {
+    "revision": "a9d036b3197cbe00db9c0dc94cecea7c",
+    "url": "/static/media/Daniel.a9d036b3.png"
+  },
+  {
+    "revision": "0c98e3da50ed2507839a8e6deab34347",
+    "url": "/static/media/irina.0c98e3da.jpeg"
+  },
+  {
+    "revision": "e6bb5eed844c155b0f32",
+    "url": "/static/css/1.31128774.chunk.css"
+  },
+  {
+    "revision": "706e1b07934734cb72f763a621128029",
+    "url": "/index.html"
+  }
+];
--- backend/public/service-worker.js
+++ backend/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0ff0fedc88c6efaa7f8c7362fb27bb03.js"
+  "/precache-manifest.50b256fb107d6c82e15211626f55c4da.js"
 );
 
 workbox.clientsClaim();
